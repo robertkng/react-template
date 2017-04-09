907e3ac23e081b927659a62990f9224eb4601bdc
Robert Ng <robertng85@gmail.com>
2017-04-08 21:59:10 -0400

    Update template

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import Nav from '../Nav/Nav.jsx';
 import './App.css';
 
 export default class App extends Component {
 
   render() {
     return (
-        <div className="App">
-      <h1>Health Hack</h1>
-        <div className="nav">
-          <Nav />
-        </div>
-        <div className="props">
-          {this.props.children}
-        </div>
+      <div className="App">
+        <h1>Hi</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './components/App/App.jsx';
-import One from './components/App/One/One.jsx';
-import Two from './components/App/Two/Two.jsx';
-import Three from './components/App/Three/Three.jsx';
 
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App} >
-      <IndexRoute component={One} />
-        <Route path="/Two" component={Two} />
-        <Route path="/Three" component={Three} />
-    </Route>
-  </Router>
-), document.querySelector('#root-container'));
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root-container')
+);
